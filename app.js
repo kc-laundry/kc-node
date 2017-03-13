562efcaba464fc9a80fd5df583dcb3ad6bd0ac91
afirdousi <anas.firdousi@gmail.com>
2017-03-13 04:14:38 -0700

    Added cors headers

--- app.js
+++ app.js
@@ -13,7 +13,21 @@ var orders = require('./routes/orders.route');
 var laundryItems = require('./routes/laundry-items.route');
 
 var app = express();
-app.use(cors());
+var methodOverride = require('method-override');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +35,12 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
